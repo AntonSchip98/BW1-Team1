c4d5807786dcd3eeda98483f4e7952fb418c5b85
AntoSchip98 <antoschip@protonmail.com>
2024-02-12 17:38:11 +0100

    commit logica timer

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,9 @@
-
 let seconds = 60;
-function timer(){
-    let interval = setInterval(function(){   
-       seconds--
-       document.querySelector("#timer1").innerHTML = seconds;
-       if(seconds === 0)
-       clearTimeout(interval)
-    }, 1000)
+function timer() {
+  let interval = setInterval(function () {
+    seconds--;
+    document.querySelector("#timer1").innerHTML = seconds;
+    if (seconds === 0) clearTimeout(interval);
+  }, 1000);
 }
-timer()
-
-
+timer();
