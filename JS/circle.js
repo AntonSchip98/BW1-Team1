f88dc540a4aeae86b650e8ce2c6039a4052691f5
DottDre <acquarius33@gmail.com>
2024-02-16 14:05:32 +0100

    commit che funziona

--- JS/circle.js
+++ JS/circle.js
@@ -1,84 +1,59 @@
-
-let risposteGiuste=5;
+let risposteGiuste = 5;
 let risposteSbagliate = 5;
 
 let risposteTotale = 10;
-
+function circle() {
+  
 
 var ctx = document.getElementById("myChart");
 var myChart = new Chart(ctx, {
-  type: 'doughnut',
+  type: "doughnut",
   data: {
-    
-    datasets: [{
-      label: '# of Tomatoes',
-      data: [risposteGiuste, risposteSbagliate,],
-      backgroundColor: [
-        '#d20094',
-        '#00ffff',
-        
-        
-      ],
-      borderColor: [
-        '#d20094',
-        '#00ffff',
-        
-       
-      ],
-      
-    }]
+    datasets: [
+      {
+        label: "# of Tomatoes",
+        data: [risposteGiuste, risposteSbagliate],
+        backgroundColor: ["#d20094", "#00ffff"],
+        borderColor: ["#d20094", "#00ffff"],
+      },
+    ],
   },
   options: {
-    
-    
-    cutout:125,
-    
+    cutout: 125,
+
     plugins: {
       tooltip: {
-        enabled: false
+        enabled: false,
       },
       legend: {
-        onHover: function() {}
-      }
-    }
-  
-  }
+        onHover: function () {},
+      },
+    },
+  },
 });
 
-
-
-
-
-
+// mettere dentro endQuiz
 /*********************calcolo risposte giuste ***********************/
-let posizioneCorette = document.getElementById('statistics-correct')
-let percentualeGiuste = risposteGiuste*100/10
+let posizioneCorette = document.getElementById("statistics-correct");
+let percentualeGiuste = (risposteGiuste * 100) / 10;
 
-posizioneCorette.append(percentualeGiuste + '%' )
+posizioneCorette.append(percentualeGiuste + "%");
 
 /*********************calcolo risposte sbagliate ***********************/
-let posizioneSbagliate = document.getElementById('statistics-wrong')
-let percentualeSbagliate = risposteSbagliate*100/10
+let posizioneSbagliate = document.getElementById("statistics-wrong");
+let percentualeSbagliate = (risposteSbagliate * 100) / 10;
 
-posizioneSbagliate.append(percentualeSbagliate + '%' )
-
-
+posizioneSbagliate.append(percentualeSbagliate + "%");
 
 /*********************quota risposte giuste ***********************/
 
+let quotaCorette = document.getElementById("number-question-correct");
 
-let quotaCorette = document.getElementById('number-question-correct')
-
-
-quotaCorette.append(risposteGiuste + '/' + risposteTotale + 'questions' )
-
+quotaCorette.append(risposteGiuste + "/" + risposteTotale + "questions");
 
 /*********************quota risposte sbagliate ***********************/
 
+let quotaSbagliate = document.getElementById("number-question-wrong");
 
-let quotaSbagliate = document.getElementById('number-question-wrong')
-
-
-quotaSbagliate.append(risposteSbagliate + '/' + risposteTotale + 'questions' )
-
-
+quotaSbagliate.append(risposteSbagliate + "/" + risposteTotale + "questions");
+}
